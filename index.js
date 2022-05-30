660eb15028981bd8c7fd66a81c81b1014491fd18
Sharan Phadke <phadkesharan@gmail.com>
2022-05-31 01:50:48 +0530

    Challenge Complete

--- index.js
+++ index.js
@@ -5,11 +5,12 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+const PORT = 8000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -22,12 +23,24 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
+app.get("/api/whoami", function(req, res) {
+
+  const response = {
+    ipaddress: req.socket.remoteAddress,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"] 
+  }
+
+  console.log(response.headers);
+  res.json(response);
+})
+
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
